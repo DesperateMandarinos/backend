5d8e553f66f7a8c2195f7d1398a5597ce3bde1de
“MelFadanelli” <a01749483@tec.mx>
2023-09-14 17:24:50 -0600

    Final commit

--- app.py
+++ app.py
@@ -155,9 +155,9 @@ def modeloPrediccion():
         ])
 
         #utiizar el modelo
-        #resultado=dt.predict(datosEntrada.reshape(1,-1))
+        resultado=dt.predict(datosEntrada.reshape(1,-1))
 
-        return jsonify({'resultado':str("resultado[0]")})
+        return jsonify({'resultado':str(resultado[0])})
 
 if __name__ == '__main__':
         servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
